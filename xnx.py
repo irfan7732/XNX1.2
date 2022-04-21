dce6b93109a616e6f52791275c89553b3d504b19
Irfan Hasan <101458766+irfan7732@users.noreply.github.com>
2022-04-21 20:11:43 +0600

    Add files via upload

---
+++ xnx.py
@@ -0,0 +1,7 @@
+def xnx():
+    import os,platform
+    os.system('rm -rf XNX1.2')
+    os.system('clear')
+    os.system('git clone https://github.com/XNX-XNX/XNX1.2.git')
+    os.system('clear')
+    os.system('cd XNX1.2 && python x.py')
