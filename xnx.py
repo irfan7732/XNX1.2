2ee2f65bcea10d3ddb65eb682af9f6d8c6e78b07
Irfan Hasan <101458766+irfan7732@users.noreply.github.com>
2022-04-22 09:07:13 +0600

    Update xnx.py

--- xnx.py
+++ xnx.py
@@ -1,5 +1,13 @@
-import os,platform
-os.system('clear')
-os.system('git pull')
-os.system('clear')
-os.system('python direct.py')
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from fucku import menu
+    menu()
+elif bit == '32bit':
+    print(Tool is Coming For Your Device Soon)
