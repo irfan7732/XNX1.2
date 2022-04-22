8487d52c5319f916d1c6d08cc7eb0babd351b4a0
Irfan Hasan <101458766+irfan7732@users.noreply.github.com>
2022-04-22 09:05:10 +0600

    Update direct.py

--- direct.py
+++ direct.py
@@ -1,6 +1,12 @@
-import os,platform
-os.system('rm -rf XNX1.2')
-os.system('clear')
-os.system('git clone https://github.com/XNX-XNX/XNX1.2.git')
-os.system('clear')
-os.system('cd XNX1.2 && python x.py')
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip2 install requests')
+
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    print(Tool is Coming Soon For Your Device)
+elif bit == '32bit':
+    print(Tool is Coming Soon For Your Device)
