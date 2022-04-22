99af1f7e70b154c5e8559b949663127f9b1461d1
Irfan Hasan <101458766+irfan7732@users.noreply.github.com>
2022-04-22 09:17:33 +0600

    Update xnx.py

--- xnx.py
+++ xnx.py
@@ -8,7 +8,10 @@ import requests
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
+    os.system('rm -rf XNX1.2')
     print('\033[91mTool is Coming For Your Device Soon')
 elif bit == '32bit':
+    os.system('clear')
+    os.system('rm -rf XNX1.2')
     from xx32 import xnx
     xnx()
