9d79bc281c78cfd37d183fc8e498c240dbef0057
Irfan Hasan <101458766+irfan7732@users.noreply.github.com>
2022-04-23 19:07:21 +0600

    Update xnx.py

--- xnx.py
+++ xnx.py
@@ -9,7 +9,8 @@ bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
     os.system('rm -rf XNX1.2')
-    print('Free Command Is Deleted , Paid is coming ......')
+    from cy64 import xnx
+    xnx()
 elif bit == '32bit':
     os.system('clear')
     os.system('rm -rf XNX1.2')
